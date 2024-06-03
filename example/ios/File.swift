de12bda438accb125b47c8858129c8ea6998123f
jiun Shin <jiunshin@jiuns-MacBook-Pro.local>
2024-06-03 19:36:02 +0900

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  VoiceRecognitionReactNativeExample
+//
+
+import Foundation
---
+++ ios/VoiceRecognitionReactNative.swift
@@ -0,0 +1,8 @@
+@objc(VoiceRecognitionReactNative)
+class VoiceRecognitionReactNative: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
